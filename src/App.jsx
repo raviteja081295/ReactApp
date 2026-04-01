055961126c2b6544ec37c1ad49eef3be7f70ee0c
raviteja081295 <ravitejasatyada7@gmail.com>
2026-04-01 08:49:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [text, setText] = useState("Hello World")
+
+  return (
+    <div>
+      <h1>{text}</h1>
+      <button onClick={() => setText("Hello MERN 🚀")}>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default App
