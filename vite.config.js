1e0d0e6261c463eea0b0a5d243f26fe06eb751ce
raviteja081295 <ravitejasatyada7@gmail.com>
2026-04-02 09:18:00 +0530

    fix: enable coverage for sonar

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -6,5 +6,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage'
+    }
   },
 })
